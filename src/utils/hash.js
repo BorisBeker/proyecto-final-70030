06befd3e927bda1b76e00490ec29cb18cf4176de
boris rafael beker <borisrafaelbeker@gmail.com>
2024-08-12 20:24:36 -0300

    m

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -11,7 +11,10 @@ export async function createHash(password){
 }
 
 export async function verifyPassword(password, hash){
-    //const isPassordCorrect = await bcrypt.compare(password, hash);
+    // const isPassordCorrect = await bcrypt.compare(password, hash); 
+    // no se por que esto no me funciona, probe con varios usuarios, coloqué varias verificaciones 
+    // para ver si estaba todo bien y por lo que vi estaba todo bien, le pregunte a chatgpt y no me 
+    // supo responder, no se que hacer pipipi T_T
     const isPassordCorrect = true;
     console.log(isPassordCorrect)
     return isPassordCorrect
